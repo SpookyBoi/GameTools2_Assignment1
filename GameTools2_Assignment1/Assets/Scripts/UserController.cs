6d435c995efaae60e3f7cd2eab279dabb79ee14f
SpookyBoi <aaronnk1999@gmail.com>
2018-10-29 22:52:56 +0000

    Walking and Sprinting
    Added in the ability to go from walking to running.

--- GameTools2_Assignment1/Assets/Scripts/UserController.cs
+++ GameTools2_Assignment1/Assets/Scripts/UserController.cs
@@ -6,6 +6,7 @@ public class UserController : MonoBehaviour {
 
     private float _Rotation;
     private float _Move;
+    private bool _Sprint;
 
     private PlayerController _Player;
 
@@ -14,10 +15,32 @@ public class UserController : MonoBehaviour {
         _Player = GetComponent<PlayerController>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _Sprint = true;
+        }
+
+        else
+        {
+            _Sprint = false;
+        }
+    }
+
     void FixedUpdate()
     {
         _Rotation = Input.GetAxis("Horizontal");
-        _Move = Input.GetAxis("Vertical");
+        //_Move = Input.GetAxis("Vertical");
+
+        if (_Sprint == true)
+        {
+            _Move = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            _Move = Mathf.Clamp(Input.GetAxis("Vertical"), -0.5f, 0.5f);
+        }
 
         _Player.Movement(_Rotation, _Move);
     }
